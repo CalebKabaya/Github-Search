2415bd996fe1a68410438c08056b53f33bd04544
calebkabaya <calebkabaya@gmail.com>
2022-04-16 10:39:18 +0300

    ft:added the search user component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { SearchUserComponent } from './search-user/search-user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainComponent,
-    NavbarComponent
+    NavbarComponent,
+    SearchUserComponent
   ],
   imports: [
     BrowserModule,
