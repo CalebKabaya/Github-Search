001d1b219c32c9f5e5715c7a8739444a01f20997
calebkabaya <calebkabaya@gmail.com>
2022-04-17 12:26:01 +0300

    ft:updated myservice to get the data for the repos

--- src/app/my-service.service.ts
+++ src/app/my-service.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { UserDetails } from './user-details';
+import { Repositories } from './repositories';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
@@ -21,10 +22,14 @@ export class MyServiceService {
 
 
   getUserDetails: any = UserDetails;
+  getUserRepositories: any = Repositories;
+
 
   constructor(private http:HttpClient) {
     this.getUserDetails = new UserDetails('','','','','',0,0,0, new Date(),'');
+    this.getUserRepositories =new Repositories('','','','',new Date());
    }
+
   //  user details
   getUserDataApi(gitUserName:string) {
     interface ApiUserResponse {
@@ -43,9 +48,6 @@ export class MyServiceService {
     let userPromise = new Promise<void>((resolve, reject) =>
     this.http.get<ApiUserResponse>(environment.apiUrl +'/' +gitUserName  +'?access_token'+environment.apiKey)
 
-    // this.http.get<ApiUserResponse>(environment.apiUrl +'/' +gitUserName  +'?access_token=' +environment.apiKey)
-      //  this.http.get('https://api.github.com/users/CalebKabaya?access_token=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')
-
         .toPromise().then(
           (response) => {
             this.getUserDetails = response;
@@ -60,5 +62,40 @@ export class MyServiceService {
         )
     );
     return userPromise;
+  }
+  getUserRepos(gitUserName:string) {
+    interface ApiUserResponse {
+      login: string,
+      avatar_url: string,
+      html_url: string,
+      name: string,
+      bio: string,
+      public_repos: number,
+      followers: number,
+      following: number,
+      created_at: Date,
+      location: string,
+    }
+
+    let repositoryPromise = new Promise<void>((resolve, reject) =>
+    this.http.get<ApiUserResponse>(environment.apiUrl +'/' +gitUserName  +'?access_token'+environment.apiKey)
+
+    // this.http.get<ApiUserResponse>(environment.apiUrl +'/' +gitUserName  +'?access_token=' +environment.apiKey)
+      //  this.http.get('https://api.github.com/users/CalebKabaya?access_token=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')
+
+        .toPromise().then(
+          (response) => {
+            this.getUserRepositories = response;
+            resolve();
+
+            console.log('Search repo',response);
+          },
+          (error) => {
+            reject(error);
+            console.log(error);
+          }
+        )
+    );
+    return repositoryPromise;
   }
 }
